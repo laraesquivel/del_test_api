82a72ce62f689c9be182e23bdedaec0c61cdb1e0
Lara Esquivel <laraesquivelsantos@gmail.com>
2025-08-04 12:52:52 -0300

    10

--- server.js
+++ server.js
@@ -2271,7 +2271,7 @@ app.get("/produtos", (req, res) => {
         "vendidomes": null,
         "dtultcomp": "07/28/2025 00:00:00"
     }
-].slice(0,30)
+].slice(0,10)
 )});
 
 app.listen(PORT, () => {
